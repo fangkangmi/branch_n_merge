2c1b1427e7c7de36ad381bf84c485a86ca4deb2e
Gurdave <gurdavebhambra@gmail.com>
2022-10-13 11:55:33 +0100

    Changed App.java

--- src/main/java/com/lbg/App.java
+++ src/main/java/com/lbg/App.java
@@ -13,6 +13,6 @@ public class App
         VATCalculator vatMaker = new VATCalculator();
         Scanner scan = new Scanner(System.in);
         System.out.print( "Enter the amount you want to calculate VAT for: " );
-        System.out.println("The VAT will be: " + vatMaker.calculateVATToPay(scan.nextDouble(),20));
+        System.out.println("VAT to pay:  " + vatMaker.calculateVATToPay(scan.nextDouble(),20));
     }
 }
