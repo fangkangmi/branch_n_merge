3c9da7f52d640bc640a0a3b0690183d7798bb0e7
Fangkang <fangkangmi@outlook.com>
2022-10-13 11:40:26 +0100

    Edited main

--- src/main/java/com/lbg/App.java
+++ src/main/java/com/lbg/App.java
@@ -14,5 +14,8 @@ public class App
         Scanner scan = new Scanner(System.in);
         System.out.print( "Enter the amount you want to calculate VAT for: " );
         System.out.println("The VAT will be: " + vatMaker.calculateVATToPay(scan.nextDouble(),20));
+        Accumulator acc = new Accumulator;
+        System.out.println("The sum of 3 and 7 is: ");
+        System.out.println(acc.sum(3,7));
     }
 }
